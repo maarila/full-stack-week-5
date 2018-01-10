bb20ae82dcc04891b7e1a8562af4440d0fbbb8b9
Mika Äärilä <maarila@gmail.com>
2018-01-10 13:16:55 +0200

    tehtävä 89

--- src/components/Togglable.js
+++ src/components/Togglable.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class Togglable extends React.Component {
   constructor(props) {
@@ -31,5 +32,9 @@ class Togglable extends React.Component {
     );
   }
 }
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired
+};
 
 export default Togglable;
