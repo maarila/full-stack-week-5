9b06be629180f561c2921a542a0c6350daff5781
Mika Äärilä <maarila@gmail.com>
2018-01-14 11:21:01 +0200

    tehtävä 134

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parser": "babel-eslint",
+    "globals": {
+        "process": true
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "eqeqeq": "error",
+        "no-console": 0,
+        "no-process-env": 0
+    }
+};
